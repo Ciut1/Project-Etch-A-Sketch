8cf293873d387984230329261e0d69e02c5430cc
ciut1 <williamyeoshuhok1st@gmail.com>
2022-02-12 15:21:42 +1100

    test

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const divContainer = document.getElementById("container")
-divContainer.style.gridTemplateRows = "repeat(16, 1fr)"
-divContainer.style.gridTemplateColumns = "repeat(16, 1fr)"
 
-for (let i = 0; i<256; i++) {
-    let square = document.createElement("div")
-    square.style.backgroundColor = "blue"
-    divContainer.insertAdjacentElement("beforeend", square)
-}
-
-/*const divHeader = document.createElement("div")
+const divHeader = document.createElement("div")
 divHeader.setAttribute("id", "header")
 divHeader.textContent = "Etch A Sketch"
 
@@ -16,7 +8,15 @@ divContainer.appendChild(divHeader)
 
 const divBoard = document.createElement("div")
 divBoard.setAttribute("id", "board")
-divBoard.textContent = "hi"
-divBoard.style.backgroundColor = "red"
 
-divContainer.appendChild(divBoard)*/
+divContainer.appendChild(divBoard)
+
+let board = document.getElementById("board")
+board.style.gridTemplateColumns = "repeat(16, 1fr)"
+board.style.gridTemplateRows = "repeat(16, 1fr)"
+
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement("div")
+    square.style.backgroundColor = "blue"
+    board.insertAdjacentElement("beforeend", square)
+}
